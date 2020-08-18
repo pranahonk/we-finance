f86f4dc5d77a12d7e5fa168fcd1bed22eee54a7c
prana.wijaya <prana.2015013004@univ.atmajaya.ac.id>
2020-08-18 08:02:53 +0700

    add carousell for card

---
+++ src/components/cardListCarousel.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="q-pa-md">
+    <q-carousel
+      v-model="slide"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      swipeable
+      animated
+      navigation
+      padding
+      arrows
+      control-color="primary"
+      class="rounded-borders"
+    >
+      <q-carousel-slide name="style" class="column no-wrap flex-center">
+        <q-icon name="style" color="primary" size="56px"/>
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="tv" class="column no-wrap flex-center">
+        <q-icon name="live_tv" color="primary" size="56px"/>
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="layers" class="column no-wrap flex-center">
+        <q-icon name="layers" color="primary" size="56px"/>
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="map" class="column no-wrap flex-center">
+        <q-icon name="terrain" color="primary" size="56px"/>
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cardListCarousel',
+  data () {
+    return {
+      slide: 'style',
+      lorem: 'Lorem ipsum dolor, sit amet consectetur test123 adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo provident incidunt ducimus iusto perferendis porro earum. Totam, numquam?'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,6 +2,7 @@
   <div>
     <date-picker />
     <main-menu />
+    <card-list-carousel />
   </div>
 
 </template>
@@ -9,12 +10,14 @@
 <script>
 import MainMenu from 'components/MainMenu'
 import datePicker from 'components/datePicker'
+import cardListCarousel from 'components/cardListCarousel'
 
 export default {
   name: 'MainLayout',
   components: {
     MainMenu,
-    datePicker
+    datePicker,
+    cardListCarousel
 
   },
   data () {
